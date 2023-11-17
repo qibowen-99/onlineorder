9e3e8b945a40bebc21b3335dcd007e2e84c4b30a
qibowen-99 <1700011744@pku.edu.cn>
2023-11-17 11:07:32 -0800

    Initial commit

---
+++ src/test/java/com/laioffer/onlineorder/OnlineOrderApplicationTests.java
@@ -0,0 +1,15 @@
+package com.laioffer.onlineorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+
+
